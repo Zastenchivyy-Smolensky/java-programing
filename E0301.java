f5a5117321aef408f37847bdc3212edc179c9ebe
Zastenchivyy-Smolensky <s20c2007@bene.fit.ac.jp>
2022-05-10 12:42:59 +0900

    ..

--- E0301.java
+++ E0301.java
@@ -10,31 +10,31 @@ public class E0301 extends JFrame { //クラスを定義している
 		setSize(200,150);//画面のサイズを指定している
 		lb1 = new JLabel("0"); //ラベルを配置している
 		lb1.setHorizontalAlignment(JLabel.CENTER);
-		bt1 = new JButton(">>");
-		bt1.addActionListener(new Up()); 
-		bt2 = new JButton("<<");
+		bt1 = new JButton(">>");//「>>」ボタンを作成している
+		bt1.addActionListener(new Up()); //upクラスを呼び出している
+		bt2 = new JButton("<<");//「>>」ボタンを作成している
 		bt2.addActionListener(new Down());
 		count = 0;//変数を初期化している
 		cnt.add(lb1, BorderLayout.CENTER); //ラベルを中央に配置している
-		cnt.add(bt1, BorderLayout.EAST);
-		cnt.add(bt2, BorderLayout.WEST);
+		cnt.add(bt1, BorderLayout.EAST);  //ボタンを左に配置している
+		cnt.add(bt2, BorderLayout.WEST);  //ボタンを右に配置している 
 		cnt.add(new JLabel("課題1 練習1 s20c2007 影山亮太"),BorderLayout.NORTH);//学生番号と名前を配置している
-		setVisible(true);
+		setVisible(true);//ウィンドウを表示させる
 	}
-	class Up implements ActionListener{
+	class Up implements ActionListener{ //「>>」ボタンが押された時に,呼び出されるクラス
 		public void actionPerformed(ActionEvent e) {
-			count++;
-			lb1.setText(Integer.toString(count));
+			count++; //countを1増やす
+			lb1.setText(Integer.toString(count));//countに格納された整数型を文字列に変換してラベル設定
 		}
 	}
-	class Down implements ActionListener{
-		public void actionPerformed(ActionEvent e) {
-			count--;
-			lb1.setText(Integer.toString(count));
+	class Down implements ActionListener{ //「<<」ボタンを押された時に呼び出されるクラス
+		public void actionPerformed(ActionEvent e) { 
+			count--;//countを1減らす
+			lb1.setText(Integer.toString(count));//countに格納された整数型を文字列に変換してラベル設定
 		}
 	}
-	public static void main(String[] args) {
-		E0301 frm = new E0301();
+	public static void main(String[] args) {//mainクラスを定義している
+		E0301 frm = new E0301();//E0301からインスタンスを作成している
 
 	}
 }

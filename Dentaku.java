a38555d503c026034e0eac98260f6fe4a5e5e5d7
Zastenchivyy-Smolensky <s20c2007@bene.fit.ac.jp>
2022-05-27 23:52:41 +0900

    ..

--- Dentaku.java
+++ Dentaku.java
@@ -1,23 +1,23 @@
-public class Dentaku {
-	int num1;
-	int num2;
-	int display;
-	String operator;
-	int state;
+public class Dentaku {//Dentakuクラスを定義している
+	int num1;//変数num1を定義している
+	int num2;//変数num2を定義している
+	int display;//変数displayを定義している
+	String operator;//変数operatorを定義している
+	int state;//変数stateを定義している
 	
-	public Dentaku() {
-		num1=0;
-		num2=0;
-		state=0;
-		display=num1;
-		operator="未";
+	public Dentaku() {//Dentakuクラスのコンストラクタ
+		num1=0;//変数num1を0に初期化している
+		num2=0;//変数num2を0に初期化している
+		state=0;//変数stateを0に初期化している
+		display=num1;//変数displayにnum1を代入している
+		operator="未";//変数operatorに文字列'未'を代入している
 	}
-	void pushNum(int n) {
-		if(state == 0 || state==3) {
+	void pushNum(int n) {//数字が押された時の挙動
+		if(state == 0 || state==3) {//初期状態,結果表示
 			num1 = n;
-			state=1;
-			display = num1;
-		}else if(state==1) {
+			state=1;//数1入力中へ遷移
+			display = num1;//
+		}else if(state==1) {//数２入力中
 			num1 = num1*10+n;
 			state =1;
 			display = num1; 
@@ -27,7 +27,7 @@ public class Dentaku {
 			display = num2;
 		}
 	}
-	void pushOp(String op) 
+	void pushOp(String op) //演算子が押された時の処理
 	{
 		if(state == 0 || state==1 || state==3) {
 			operator =op; 
@@ -41,7 +41,7 @@ public class Dentaku {
 			display = num1; 
 		}
 	}
-	void pushClear()
+	void pushClear() //クリアが押された時の処理
 	{
 		num1 = 0;
 		num2 = 0;
@@ -49,7 +49,7 @@ public class Dentaku {
 		display = num1;
 		operator = "未";
 	}
-	void pushCalc()
+	void pushCalc() //「=」が押された時の処理
 	{
 		if(state == 0 || state==1 || state==3) {
 			state = 3;
@@ -60,56 +60,56 @@ public class Dentaku {
 			state = 3;
 			display = num1;
 		}
-		operator="未";
+		operator="未";//誤りを防ぐための追加
 	}
-	String output() {
+	String output() {//結果表示用のメソッド
 		return Integer.toString(display);
 	}
-	int calc()
+	int calc()//operatorに保持されている演算子に応じて,num1,num2の演算を行い,結果を戻り値とする
 	{
-		if(operator.equals("+")) {
-			return num1+num2;
+		if(operator.equals("+")) {//「+」が押された時の処理
+			return num1+num2;//num1+num2の計算を行う
 		}
-		else if(operator.equals("-")) {
-			return num1-num2;
+		else if(operator.equals("-")) {//「-」が押された時の処理
+			return num1-num2;//num1-num2の計算を行う
 		}
-		else if(operator.equals("*")) {
-			return num1*num2;
+		else if(operator.equals("*")) {//「*」が押された時の処理
+			return num1*num2;//num1*num2の計算を行う
 		}
-		else if(operator.equals("/")) {
-			return num1/num2;
+		else if(operator.equals("/")) {//「/」が押された時の処理
+			return num1/num2;//num1/num2の計算を行う
 		}
-		return  0;
+		return  0;//エラー処理
 	}
-	void show()
+	void show()//電卓の現在の状態を表示
 	{
-		System.out.println("state:"+state+" num1:"+num1+" num2:"+num2+" operator:"+operator+" display:"+display);
+		System.out.println("state:"+state+" num1:"+num1+" num2:"+num2+" operator:"+operator+" display:"+display);//コンソール上に現在の状態を表示
 	}
 	
 	
 
-	public static void main(String[] args) {
-		Dentaku d = new Dentaku();
-		d.pushNum(1);
-		d.show();
-		d.pushNum(2);
-		d.show();
-		d.pushNum(3);
-		d.show();
-		d.pushOp("+");
-		d.show();
-		d.pushNum(4);
-		d.show();
-		d.pushNum(5);
-		d.show();
-		d.pushCalc();
-		d.show();
-		d.pushNum(1);
-		d.show();
-		d.pushNum(2);
-		d.show();
-		d.pushNum(3);
-		d.show();
+	public static void main(String[] args) {//mainメソッドで動作テストを行う
+		Dentaku d = new Dentaku();//Dentakuクラスのオブジェクトの作成
+		d.pushNum(1);//1ボタンが押された時
+		d.show();//1ボタンが押された時の結果処理
+		d.pushNum(2);//2ボタンが押された時
+		d.show();//2ボタンが押された時の結果処理
+		d.pushNum(3);//3ボタンが押された時
+		d.show();//3ボタンが押された押された時の結果表示
+		d.pushOp("+");//+ボタンが押された時
+		d.show();//+ボタンが押された押された時の結果表示
+		d.pushNum(4);//4ボタンが押された時
+		d.show();//4ボタンが押された押された時の結果表示
+		d.pushNum(5);//5ボタンが押された時
+		d.show();//5ボタンが押された押された時の結果表示
+		d.pushCalc();//=ボタンが押された時
+		d.show();//=ボタンが押された押された時の結果表示
+		d.pushNum(1);//1ボタンが押された時
+		d.show();//1ボタンが押された押された時の結果表示
+		d.pushNum(2);//2ボタンが押された時
+		d.show();//2ボタンが押された押された時の結果表示
+		d.pushNum(3);//3ボタンが押された時
+		d.show();//3ボタンが押された押された時の結果表示
 		System.out.println("=>"+d.output());
 	}
 
--- DentakuGUI2.java
+++ DentakuGUI2.java
@@ -2,113 +2,88 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 public class DentakuGUI2 extends JFrame implements ActionListener {
-	Dentaku dtk;
-	JTextField tf;
-	JPanel keypad;
-	JButton bt0, bt1,bt2,bt3,bt4,bt5, bt6,bt7,bt8,bt9, btPlus,btMinus,btMui, btDiv,btClear,btCalc;
-	public DentakuGUI2() {
-		dtk = new Dentaku();
-		Container cp = getContentPane();
-		tf = new JTextField();
-		tf.setHorizontalAlignment(SwingConstants.RIGHT);
+	Dentaku dtk; //Dentakuクラスのオブジェクトを格納する変数
+	JTextField tf;//結果を表示するためのテキストフィールド
+	JPanel keypad;//ボタン配置用のパネル
+	JButton bt0, bt1,bt2,bt3,bt4,bt5, bt6,bt7,bt8,bt9, btPlus,btMinus,btMui, btDiv,btClear,btCalc;//操作用のボタン配置のパネル
+	public DentakuGUI2() {//DentakuGUI2クラスのコンストラクター
+		dtk = new Dentaku();// Dentakuクラスのオブジェクトを作成
+		Container cp = getContentPane();//コンテンツペインの習得
+		tf = new JTextField();//結果表示用のテキストフィールド
+		tf.setHorizontalAlignment(SwingConstants.RIGHT);//表示を右よせにする
 		tf.setText(dtk.output());
-		keypad = new JPanel();
-		keypad.setLayout(new GridLayout(4,4));
-		bt0 = new JButton("0");
-		bt0.addActionListener(this);
-		bt1 = new JButton("1");
-		bt1.addActionListener(this);
-		bt2 = new JButton("2");
-		bt2.addActionListener(this);
-		bt3 = new JButton("3");
-		bt3.addActionListener(this);
-		bt4 = new JButton("4");
-		bt4.addActionListener(this);
-		bt5 = new JButton("5");
-		bt5.addActionListener(this);
-		bt6 = new JButton("6");
-		bt6.addActionListener(this);
-		bt7 = new JButton("7");
-		bt7.addActionListener(this);
-		bt8 = new JButton("8");
-		bt8.addActionListener(this);
-		bt9 = new JButton("9");
-		bt9.addActionListener(this);
-		btPlus = new JButton("+");
-		btPlus.addActionListener(this);
-		btMinus = new JButton("-");
-		btMinus.addActionListener(this);
-		btMui = new JButton("*");
-		btMui.addActionListener(this);
-		btDiv = new JButton("/");
-		btDiv.addActionListener(this);
-		btClear = new JButton("C");
-		btClear.addActionListener(this);
-		btCalc = new JButton("=");
-		btCalc.addActionListener(this);
-		keypad.add(bt7);keypad.add(bt8);keypad.add(bt9);keypad.add(btMinus);
-		keypad.add(bt4);keypad.add(bt5);keypad.add(bt6);keypad.add(btPlus);
-		keypad.add(bt1);keypad.add(bt2);keypad.add(bt3);keypad.add(btMui);
-		keypad.add(btCalc);
-		keypad.add(bt0);
-		keypad.add(btClear);
-		keypad.add(btDiv);
-		cp.add(tf, BorderLayout.NORTH);
-		cp.add(keypad, BorderLayout.CENTER);
-		cp.add(new JLabel("作成者 20c2007 影山亮太"),BorderLayout.SOUTH);
-		pack();setVisible(true);
+		keypad = new JPanel();//JPanelクラスからインスタンスを作成
+		keypad.setLayout(new GridLayout(4,4));//レチアウトマネージャの設定
+		bt0 = new JButton("0");bt0.addActionListener(this);  bt1 = new JButton("1");bt1.addActionListener(this);//JButtonクラスからインスタンスを作成している
+		bt2 = new JButton("2");bt2.addActionListener(this);bt3 = new JButton("3");bt3.addActionListener(this);;//JButtonクラスからインスタンスを作成している
+		bt4 = new JButton("4");bt4.addActionListener(this);bt5 = new JButton("5");bt5.addActionListener(this);;//JButtonクラスからインスタンスを作成している
+		bt6 = new JButton("6");bt6.addActionListener(this);bt7 = new JButton("7");bt7.addActionListener(this);;//JButtonクラスからインスタンスを作成している
+		bt8 = new JButton("8");bt8.addActionListener(this);bt9 = new JButton("9");bt9.addActionListener(this);;//JButtonクラスからインスタンスを作成している
+		btPlus = new JButton("+");btPlus.addActionListener(this);btMinus = new JButton("-");btMinus.addActionListener(this);;//JButtonクラスからインスタンスを作成している
+		btMui = new JButton("*");btMui.addActionListener(this);btDiv = new JButton("/");btDiv.addActionListener(this);;//JButtonクラスからインスタンスを作成している
+		btClear = new JButton("C");btClear.addActionListener(this);btCalc = new JButton("=");btCalc.addActionListener(this);;//JButtonクラスからインスタンスを作成している
+		
+		keypad.add(bt7);keypad.add(bt8);keypad.add(bt9);keypad.add(btMinus);//電卓のボタンを配置している
+		keypad.add(bt4);keypad.add(bt5);keypad.add(bt6);keypad.add(btPlus);//電卓のボタンを配置している
+		keypad.add(bt1);keypad.add(bt2);keypad.add(bt3);keypad.add(btMui);//電卓のボタンを配置している
+		keypad.add(btCalc);keypad.add(bt0);keypad.add(btClear);keypad.add(btDiv);//電卓のボタンを配置している
+		cp.add(tf, BorderLayout.NORTH);//計算結果の画面を配置してフレームを表示している
+		cp.add(keypad, BorderLayout.CENTER);//ボタンを中央に配置してフレームを表示している
+		cp.add(new JLabel("作成者 20c2007 影山亮太"),BorderLayout.SOUTH);//学籍番号と氏名を配置してフレームに表示している
+		pack();setVisible(true);//フレームが表示されるようにしている.
 	}
+	//ボタンが押された時の処理
 	public void actionPerformed(ActionEvent e) {
-		String cmd = e.getActionCommand();
-		if(cmd.contentEquals("C")) {
-			dtk.pushClear();
+		String cmd = e.getActionCommand();//押されたボタンのラベルを習得
+		if(cmd.contentEquals("C")) {//Cが押された時の処理
+			dtk.pushClear();//表示された数値をclearにする
 		}
-		else if(cmd.contentEquals("=")) {
-			dtk.pushCalc();
+		else if(cmd.contentEquals("=")) {//「=」が押された時の処理
+			dtk.pushCalc();//結果を表示してくれる
 		}
-		else if(cmd.contentEquals("+")) {
-			dtk.pushOp("+");
+		else if(cmd.contentEquals("+")) {//「+」が押された時の処理
+			dtk.pushOp("+");//足し算の処理を行う
 		}
-		else if(cmd.contentEquals("-")) {
-			dtk.pushOp("-");
+		else if(cmd.contentEquals("-")) {//「-」が押された時の処理
+			dtk.pushOp("-");//引き算の処理を行う
 		}
-		else if(cmd.contentEquals("*")) {
-			dtk.pushOp("*");
+		else if(cmd.contentEquals("*")) {//「*」が押された時の処理
+			dtk.pushOp("*");//掛け算の処理を行う
 		}
-		else if(cmd.contentEquals("/")) {
-			dtk.pushOp("/");
+		else if(cmd.contentEquals("/")) {//「/」が押された時の処理
+			dtk.pushOp("/");//割り算の処理を行う
 		}
-		else if(cmd.contentEquals("0")) {
-			dtk.pushNum(0);
+		else if(cmd.contentEquals("0")) {//「0」が押された時の処理
+			dtk.pushNum(0);//数値表示欄に「0」を表示する
 		}
-		else if(cmd.contentEquals("1")) {
-			dtk.pushNum(1);
+		else if(cmd.contentEquals("1")) {//「1」が押された時の処理
+			dtk.pushNum(1);//数値表示欄に「1」を表示する
 		}
-		else if(cmd.contentEquals("2")) {
-			dtk.pushNum(2);
+		else if(cmd.contentEquals("2")) {//「2」が押された時の処理
+			dtk.pushNum(2);//数値表示欄に「2」を表示する
 		}
-		else if(cmd.contentEquals("3")) {
-			dtk.pushNum(3);
+		else if(cmd.contentEquals("3")) {//「3」が押された時の処理
+			dtk.pushNum(3);//数値表示欄に「3」を表示する
 		}
-		else if(cmd.contentEquals("4")) {
-			dtk.pushNum(4);
+		else if(cmd.contentEquals("4")) {//「4」が押された時の処理
+			dtk.pushNum(4);//数値表示欄に「4」を表示する
 		}
-		else if(cmd.contentEquals("5")) {
-			dtk.pushNum(5);
+		else if(cmd.contentEquals("5")) {//「5」が押された時の処理
+			dtk.pushNum(5);//数値表示欄に「5」を表示する
 		}
-		else if(cmd.contentEquals("6")) {
-			dtk.pushNum(6);
+		else if(cmd.contentEquals("6")) {//「6」が押された時の処理
+			dtk.pushNum(6);//数値表示欄に「6」を表示する
 		}
-		else if(cmd.contentEquals("7")) {
-			dtk.pushNum(7);
+		else if(cmd.contentEquals("7")) {//「7」が押された時の処理
+			dtk.pushNum(7);//数値表示欄に「7」を表示する
 		}
-		else if(cmd.contentEquals("8")) {
-			dtk.pushNum(8);
+		else if(cmd.contentEquals("8")) {//「8」が押された時の処理
+			dtk.pushNum(8);//数値表示欄に「8」を表示する
 		}
-		else if(cmd.contentEquals("9")) {
-			dtk.pushNum(9);
+		else if(cmd.contentEquals("9")) {//「9」が押された時の処理
+			dtk.pushNum(9);//数値表示欄に「9」を表示する
 		}
-		tf.setText(dtk.output());
+		tf.setText(dtk.output());//結果をテキストフィールドに設定
 	}
 	public static void main(String[] args) {
 		DentakuGUI2 dtk_gui = new DentakuGUI2();
